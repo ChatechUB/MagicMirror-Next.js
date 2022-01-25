2bd1aa80bd7ee6e99b0fc303e799a8599b8cc280
petterwennberg <petter.wwennberg@gmail.com>
2022-01-25 19:14:28 +0100

    Clock update

--- components/Clock.js
+++ components/Clock.js
@@ -2,7 +2,11 @@ import React, {useEffect, useState} from 'react'
 
 function Clock ()  {
     
+    let days = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"]
+    let months = ["januar", "februar", "mars", "april", "mai", "juni", "juli", "august", "september", "oktober", "november", "desember"]
+
     const [clock, setClock] = useState();
+    const [date, setDate] = useState();
 
     
         setInterval(function () {
@@ -11,16 +15,31 @@ function Clock ()  {
             let m = today.getMinutes();
             let s = today.getSeconds();
 
-            
+ 
             setClock(h + ":" + m + ":" + s);
+            
+            if (s == 0) {
+                let day = today.getDay();
+                let month = today.getMonth();
+                let d = today.getDate();
+
+                setDate(days[day] + " " + d + "." + months[month]);
+            }
+
+
+
+
+            
     
         }, 1000);
+
 
 
 
     return (
         <div>
             <h1 className="clock">{clock}</h1>
+            <h2 className="date">{date}</h2>
         </div>
     )
 }
