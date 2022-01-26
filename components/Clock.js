60e7f2f15323b17468e8b5df2a0e3df860bc8d12
petterwennberg <89514539+petterwennberg@users.noreply.github.com>
2022-01-26 11:21:36 +0100

    fixed clock

--- components/Clock.js
+++ components/Clock.js
@@ -5,20 +5,20 @@ function Clock ()  {
     let days = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"]
     let months = ["januar", "februar", "mars", "april", "mai", "juni", "juli", "august", "september", "oktober", "november", "desember"]
 
-    const [clock, setClock] = useState();
+    const [clock, setClock] = useState("00:00:00");
     const [date, setDate] = useState();
 
     
         setInterval(function () {
             const today = new Date();
-            let h = today.getHours();
-            let m = today.getMinutes();
-            let s = today.getSeconds();
+            let h = today.getHours().toString().padStart(2, "0");
+            let m = today.getMinutes().toString().padStart(2, "0");
+            let s = today.getSeconds().toString().padStart(2, "0");
 
  
-            setClock(h + ":" + m + ":" + s);
+            setClock(`${h}:${m}:${s}`);
             
-            if (s == 0) {
+            if (s === "00") {
                 let day = today.getDay();
                 let month = today.getMonth();
                 let d = today.getDate();
