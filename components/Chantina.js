6e7eda3a12ff801d594a66e8cbb5ffb40cf86afd
petterwennberg <89514539+petterwennberg@users.noreply.github.com>
2022-01-28 13:52:50 +0100

    Chantina done

--- components/Chantina.js
+++ components/Chantina.js
@@ -8,9 +8,9 @@ export const Chantina = ({ matretter, priser }) => {
 	const [data, setData] = useState([])
 
     useEffect(() => {
-		axios.get("https://chantina.no/api/getWeek")
+		axios.get("https://api.chantina.no/getWeek")
 		.then((res)=> {
-			console.log(res.data);
+			console.log(res.data[new Date().getDay() - 1].Pris);
 			setData(res.data)
 		})
 	}, [])
@@ -19,8 +19,8 @@ export const Chantina = ({ matretter, priser }) => {
     return (
         <div className={styles.chantinaParent}>
             <h1 className={styles.chantina}>Dagens Lunsj er</h1>
-			<h1 className={styles.chantina}>{data[new Date().getDay() - 1].matretter}</h1>
-			<h1 className={styles.chantina}>{data[new Date().getDay() - 1].priser}</h1>
+			<h1 className={styles.chantina}>{data[new Date().getDay() - 1].Matrett}</h1>
+			<h1 className={styles.chantina}>{data[new Date().getDay() - 1].Pris}</h1>
 			
         </div>
     )
